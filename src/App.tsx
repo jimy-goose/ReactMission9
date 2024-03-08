a4401b0d74f9f0ff49e2253b354c7077f532b2d3
jimy-goose <131339722+jimy-goose@users.noreply.github.com>
2024-03-08 15:40:44 -0700

    I set up react

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import CollegeBasketballTeams from './CollegeBasketballTeams.json';
 import './App.css';
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return <h1>The Best Music Trios</h1>;
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+    return (
+      <div>
+        <img />
+        <h2>{oneTeam.school}</h2>
+        <h3>Name: {oneTeam.name}</h3>
+        <h3>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {CollegeBasketballTeams.teams.map((teamNum) => (
+        <Team {...teamNum} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
